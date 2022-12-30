4ec3002d727638f2cc9237043565c5289f1fe46f
John Gualteros <gualterosjohn40@gmail.com>
2022-12-30 09:46:23 -0500

    added the send email with registration user

--- src/main/java/com/john/hexagonal_architecture/user/infrastructure/controller/UserPostController.java
+++ src/main/java/com/john/hexagonal_architecture/user/infrastructure/controller/UserPostController.java
@@ -1,7 +1,9 @@
 package com.john.hexagonal_architecture.user.infrastructure.controller;
 
+import com.john.hexagonal_architecture.user.application.EmailSenderService;
 import com.john.hexagonal_architecture.user.application.UserRepository;
 import com.john.hexagonal_architecture.user.application.ValidateIfEmailAlreadyExists;
+import com.john.hexagonal_architecture.user.domain.Email;
 import com.john.hexagonal_architecture.user.domain.UserDto;
 import com.john.hexagonal_architecture.user.infrastructure.UserEntity;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.mail.MessagingException;
 import java.util.Optional;
 
 @RestController
@@ -24,10 +27,12 @@ public class UserPostController {
 
     private final PasswordEncoder              passwordEncoder;
 
+    private final EmailSenderService           emailService;
+
     private final ValidateIfEmailAlreadyExists validateIfEmailAlreadyExists;
 
     @PostMapping("/users")
-    public ResponseEntity<UserEntity> saveOneUser (@RequestBody UserDto userDto) {
+    public ResponseEntity<UserEntity> saveOneUser (@RequestBody UserDto userDto) throws MessagingException {
 
         UserEntity user = convertDtoToModelUser(userDto);
 
@@ -37,6 +42,17 @@ public class UserPostController {
         if(userAlreadyExists.isPresent()){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists!");
         }
+
+        String message = "muchas gracias por registrarte en nuestro sistema \na continuacion encontraras el usuario para que puedas iniciar sesion: " + user.getName();
+
+        Email email = new Email(
+                user.getEmail(),
+                message,
+                "Registro exitoso en Jbugs",
+                "\"C:\\\\Users\\\\jumbo170\\\\Downloads\\\\github.webp\""
+        );
+
+        emailService.sendEmailWithAttachment(email);
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
